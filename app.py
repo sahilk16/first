bceb5a597cec2fd760c544315fd01eb1c7380712
rohan.mathure <rohan.mathure@hulu.com>
2016-01-07 11:14:22 -0800

    Adding blueprint to the  app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restful import Api
 from resources.user import User
 
 app = Flask(__name__)
+api_blueprint = Blueprint('api', __name__)
 api = Api(app)
 
 api.add_resource(User, '/User', '/user/<int:id>')
+app.register_blueprint(api_blueprint)
