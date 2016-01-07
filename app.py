df71032d2fda039aa2ba8e2119e9485c61068cf4
rohan.mathure <rohan.mathure@hulu.com>
2016-01-07 11:10:00 -0800

    Adding structure to the project.

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_restful import Api
+from resources.user import User
 
 app = Flask(__name__)
+api = Api(app)
 
-
-@app.route('/')
-def hello_world():
-    return render_template("Main.html")
-
-
-if __name__ == '__main__':
-    app.run()
+api.add_resource(User, '/User', '/user/<int:id>')
